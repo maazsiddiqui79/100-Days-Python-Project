a5d46d0b2788514f9cf9092fb66542ba65611075
Maaz Irshad Siddiqui <85389149+maazsiddiqui79@users.noreply.github.com>
2025-06-27 17:34:34 +0530

    Added a project rental web scraper with form auto-fill using Selenium

---
+++ 54. Day53 - Rental Web Scraper [Cap Stone Project]/collecting_data.py
@@ -0,0 +1,109 @@
+from bs4 import BeautifulSoup
+import os
+import pandas
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+
+
+class Rental_Web_Scraper ():
+    def __init__(self,folder_path):
+        self.folder_path = folder_path
+        self.data ={
+            'Address': [],
+            'Price': [],
+            'Link': [],
+        }
+        self.read_file()
+        self.save_data()
+
+        
+        
+    def read_file(self):
+        
+        for i in os.listdir(self.folder_path):
+            file_path = os.path.join(self.folder_path,i)
+            with open(file_path,'r',encoding="utf-8") as f:
+                html_doc = f.read()
+                
+            self.extract_data(html_document=html_doc)
+                
+                
+    def extract_data (self,html_document):
+        soup = BeautifulSoup(html_document,'html.parser')
+        
+        try:
+            # Address
+            Add = soup.find('address').getText()
+            
+            if Add:
+                self.data['Address'].append(Add.strip())
+            else:
+                self.data['Address'].append('N/A')
+            
+            # Price
+            Price = soup.find('span',class_='PropertyCardWrapper__StyledPriceLine').getText()
+            if Price:
+                self.data['Price'].append(Price)
+            else:
+                self.data['Price'].append('N/A')
+                
+            # Link
+            Link = soup.find('a',class_='property-card-link').get_attribute_list('href')
+            Link = Link[0]+'.html'
+            
+            if Link:
+                self.data['Link'].append(Link)
+            else:
+                self.data['Link'].append('N/A')
+            
+            
+            
+        except Exception as e:
+            print('E R R O R',e)
+            
+            
+    def save_data (self):
+        data1 = pandas.DataFrame(self.data)
+        for k,v in data1.iterrows():
+
+            
+            chrome_options = webdriver.ChromeOptions()
+            chrome_options.add_experimental_option("detach",True)
+            driver =webdriver.Chrome(options=chrome_options)
+            driver.get(url="https://forms.gle/BLQq5FsVhkbBrPq7A") 
+            a1 = v.Address
+            print(a1)
+            
+            p1 = v.Price
+            print(p1)
+            
+            l1 = v.Link
+            print(l1)
+            
+            wait = WebDriverWait(driver, 10)
+            q1 = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')))
+            q2 = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')))
+            q3 = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')))
+            btn = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span')))
+
+            q1.send_keys(a1)
+            q2.send_keys(p1)
+            q3.send_keys(l1)
+            btn.click()
+            time.sleep(1)
+            
+            driver.close()
+        
+        
+        
+            
+        
+        
+        
+d = Rental_Web_Scraper("database")  
+
+    
---
+++ 54. Day53 - Rental Web Scraper [Cap Stone Project]/main.py
@@ -0,0 +1,35 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+from collecting_data import Rental_Web_Scraper
+
+URL ='https://appbrewery.github.io/Zillow-Clone/'
+
+chrome = webdriver.ChromeOptions()
+chrome.add_experimental_option("detach",True)
+
+driver = webdriver.Chrome(options=chrome)
+driver.get(url=URL)
+
+elements = driver.find_elements(by=By.CLASS_NAME,value="StyledCard-c11n-8-84")
+print(elements)
+
+file = 0
+for i in elements:
+    
+    data = i.get_attribute("outerHTML")
+    with open(f"database\Rental_Rooms_{file}.html","w",encoding="utf-8") as f :
+        f.write(data)
+        file+=1
+    
+    print(i)
+    print(i.tag_name)
+    print(i.text)
+    print("+-------------------------------+")
+    
+    
+rent = Rental_Web_Scraper(folder_path='database')
